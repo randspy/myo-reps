76b2cae8c76d46e0e6525a993cf3673d8ebec3fe
Przemyslaw Koziel <mos.koziel@gmail.com>
2024-08-11 09:20:20 +0200

    Router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Outlet } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 
 export const App = () => (
   <div className="h-dvh">
     <Button> Hello </Button>
+    <Outlet />
   </div>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './index.css';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
+import Workouts from './app/workouts/Workouts';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Navigate to="/workouts" />,
+      },
+      {
+        path: '/workouts',
+        element: <Workouts />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
