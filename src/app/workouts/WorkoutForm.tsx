8806dccd9e48912fc5b3ce53fbafa151fb73e3a8
Przemyslaw Koziel <mos.koziel@gmail.com>
2024-08-22 16:20:52 +0200

    feat: Drag and drop in Workout form

--- src/app/workouts/WorkoutForm.tsx
+++ src/app/workouts/WorkoutForm.tsx
@@ -21,6 +21,8 @@ import { ExerciseComboBox } from '@/app/workouts/ExerciseComboBox';
 import { v4 as uuidv4 } from 'uuid';
 import { NumberScrollWheelSelectorPopover } from '@/app/common/NumberScrollWheelSelectorPopover';
 import { Trash2Icon } from 'lucide-react';
+import { Reorder } from 'framer-motion';
+import { useState } from 'react';
 
 export const WorkoutForm: React.FC<{
   onSubmit: (values: WorkoutFormValues) => void;
@@ -31,10 +33,11 @@ export const WorkoutForm: React.FC<{
     values,
   });
 
-  const { fields, append } = useFieldArray({
+  const { fields, append, move } = useFieldArray({
     name: 'exercises',
     control: form.control,
   });
+  const [active, setActive] = useState(0);
 
   const exercises = useAppSelector((state) => state.exercises.values);
 
@@ -78,61 +81,79 @@ export const WorkoutForm: React.FC<{
         {fields.length > 0 && (
           <FormLabel className="mb-2 inline-block">Exercises</FormLabel>
         )}
-        {fields.map((exercise, index) => (
-          <div key={exercise.id}>
-            <FormField
-              control={form.control}
-              key={index}
-              name={`exercises.${index}.exerciseId`}
-              render={() => (
-                <FormItem className="mb-2">
-                  <FormControl>
-                    <div className="grid w-full grid-cols-[1fr,minmax(7rem,auto)] space-x-2">
-                      <ExerciseComboBox
-                        items={exercises}
-                        selected={form.getValues(`exercises.${index}`)}
-                        onSelect={(item) => {
-                          form.setValue(
-                            `exercises.${index}.exerciseId`,
-                            item.exerciseId,
-                          );
-                          form.clearErrors(`exercises.${index}.exerciseId`);
-                        }}
-                      />
-                      <div className="flex justify-end">
-                        <NumberScrollWheelSelectorPopover
-                          value={form.getValues(`exercises.${index}.sets`)}
-                          label="Sets"
-                          max={20}
-                          onValueChange={(value) => {
-                            form.setValue(`exercises.${index}.sets`, value);
-                            form.clearErrors(`exercises.${index}.sets`);
+        <Reorder.Group
+          values={fields}
+          onReorder={(exercises) => {
+            exercises.forEach((exercise, index) => {
+              const activeExercise = fields[active];
+              if (exercise === activeExercise) {
+                move(active, index);
+                setActive(index);
+              }
+            });
+          }}
+        >
+          {fields.map((exercise, index) => (
+            <Reorder.Item
+              key={exercise.id}
+              value={exercise}
+              id={exercise.id}
+              onDragStart={() => setActive(index)}
+            >
+              <FormField
+                control={form.control}
+                key={index}
+                name={`exercises.${index}.exerciseId`}
+                render={() => (
+                  <FormItem className="mb-2">
+                    <FormControl>
+                      <div className="grid w-full grid-cols-[1fr,minmax(7rem,auto)] space-x-2">
+                        <ExerciseComboBox
+                          items={exercises}
+                          selected={form.getValues(`exercises.${index}`)}
+                          onSelect={(item) => {
+                            form.setValue(
+                              `exercises.${index}.exerciseId`,
+                              item.exerciseId,
+                            );
+                            form.clearErrors(`exercises.${index}.exerciseId`);
                           }}
                         />
-                        <Button
-                          type="button"
-                          className="ml-2"
-                          aria-label="Delete exercise"
-                          variant="icon"
-                          size="icon"
-                          onClick={() => {
-                            form.setValue(
-                              'exercises',
-                              fields.filter((_, i) => i !== index),
-                            );
-                          }}
-                        >
-                          <Trash2Icon className="h-4 w-4" />
-                        </Button>
+                        <div className="flex justify-end">
+                          <NumberScrollWheelSelectorPopover
+                            value={form.getValues(`exercises.${index}.sets`)}
+                            label="Sets"
+                            max={20}
+                            onValueChange={(value) => {
+                              form.setValue(`exercises.${index}.sets`, value);
+                              form.clearErrors(`exercises.${index}.sets`);
+                            }}
+                          />
+                          <Button
+                            type="button"
+                            className="ml-2"
+                            aria-label="Delete exercise"
+                            variant="icon"
+                            size="icon"
+                            onClick={() => {
+                              form.setValue(
+                                'exercises',
+                                fields.filter((_, i) => i !== index),
+                              );
+                            }}
+                          >
+                            <Trash2Icon className="h-4 w-4" />
+                          </Button>
+                        </div>
                       </div>
-                    </div>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-        ))}
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </Reorder.Item>
+          ))}
+        </Reorder.Group>
 
         <Button
           className="mb-4 mt-2 w-full"
