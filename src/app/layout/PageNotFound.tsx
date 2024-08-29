e9785dd5c98ca40a2d8528b3b35466f19332ae25
Przemyslaw Koziel <mos.koziel@gmail.com>
2024-08-29 08:33:00 +0200

    feat: Page not found text size increase

--- src/app/layout/PageNotFound.tsx
+++ src/app/layout/PageNotFound.tsx
@@ -1,6 +1,6 @@
 export const PageNotFound: React.FC = () => {
   return (
-    <div className="flex h-full items-center justify-center">
+    <div className="flex h-full items-center justify-center text-3xl">
       Page not found
     </div>
   );
