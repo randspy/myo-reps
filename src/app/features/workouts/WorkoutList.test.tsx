054455a0f22c2486b0f95f96a082df5aaf3fca13
Przemyslaw Koziel <mos.koziel@gmail.com>
2024-09-12 11:50:05 +0200

    refactor: improved the unit tests for WorkoutList component

--- src/app/features/workouts/WorkoutList.test.tsx
+++ src/app/features/workouts/WorkoutList.test.tsx
@@ -1,13 +1,8 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import workoutsReducer from '@/app/core/workouts/store/workouts-slice';
-import exercisesReducer from '@/app/core/exercises/store/exercises-slice';
+import { fireEvent, screen } from '@testing-library/react';
 import { WorkoutList } from './WorkoutList';
 import { WorkoutValue } from '@/app/core/workouts/workouts-schema';
-import { generateWorkout } from '@/lib/test-utils';
+import { generateWorkout, renderWithProviders } from '@/lib/test-utils';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
-import { RootState } from '@/store/store';
 
 const deleteWorkout = vi.fn();
 
@@ -18,85 +13,71 @@ vi.mock('@/app/features/workouts/DeleteWorkoutDialog', () => ({
   },
 }));
 
-const renderWorkoutList = (preloadedState: RootState) => {
-  const store = configureStore({
-    reducer: {
-      workouts: workoutsReducer,
-      exercises: exercisesReducer,
+const renderWorkoutList = (workouts: WorkoutValue[]) => {
+  return renderWithProviders(
+    <MemoryRouter initialEntries={['/workouts']}>
+      <Routes>
+        <Route path="/workouts" element={<WorkoutList />} />
+        <Route path="/workouts/:id" element={<div>Mock Edit Workout</div>} />
+        <Route path="/sessions/new" element={<div>Mock New Session</div>} />
+      </Routes>
+    </MemoryRouter>,
+    {
+      preloadedState: {
+        workouts: {
+          values: workouts,
+        },
+      },
     },
-    preloadedState,
-  });
-
-  render(
-    <Provider store={store}>
-      <MemoryRouter initialEntries={['/workouts']}>
-        <Routes>
-          <Route path="/workouts" element={<WorkoutList />} />
-          <Route path="/workouts/:id" element={<div>Mock Edit Workout</div>} />
-          <Route path="/sessions/new" element={<div>Mock New Session</div>} />
-        </Routes>
-      </MemoryRouter>
-    </Provider>,
   );
 };
 
 describe('Workout list', () => {
-  let preloadedState: RootState;
+  let workouts: WorkoutValue[];
 
   beforeEach(() => {
-    preloadedState = {
-      exercises: {
-        values: [],
-      },
-      workouts: {
-        values: [
-          generateWorkout({
-            id: '1',
-            position: 0,
-            name: 'Upper body',
-            exercises: [],
-          }),
-        ],
-      },
-    };
+    workouts = [
+      generateWorkout({
+        id: '1',
+        position: 0,
+        name: 'Upper body',
+        exercises: [],
+      }),
+    ];
   });
 
   test('renders the list of workouts', () => {
-    renderWorkoutList(preloadedState);
+    renderWorkoutList(workouts);
 
-    preloadedState.workouts.values.forEach((workouts) => {
-      expect(screen.getByText(workouts.name)).toBeInTheDocument();
+    workouts.forEach((workout) => {
+      expect(screen.getByText(workout.name)).toBeInTheDocument();
     });
   });
 
   test('pass through workouts to child components', async () => {
-    renderWorkoutList(preloadedState);
+    renderWorkoutList(workouts);
 
-    expect(deleteWorkout).toHaveBeenCalledWith(
-      preloadedState.workouts.values[0],
-    );
+    expect(deleteWorkout).toHaveBeenCalledWith(workouts[0]);
   });
 
   test('redirect to edit page', () => {
-    renderWorkoutList(preloadedState);
+    renderWorkoutList(workouts);
 
     fireEvent.click(screen.getAllByLabelText('Edit workout')[0]);
     expect(screen.getByText('Mock Edit Workout')).toBeInTheDocument();
   });
 
   test('redirect to new session page for a given workout', () => {
-    preloadedState.workouts.values[0].exercises = [
-      { id: '1', exerciseId: '1', sets: 3 },
-    ];
+    workouts[0].exercises = [{ id: '1', exerciseId: '1', sets: 3 }];
 
-    renderWorkoutList(preloadedState);
+    renderWorkoutList(workouts);
 
     fireEvent.click(screen.getByLabelText('Start workout'));
     expect(screen.getByText('Mock New Session')).toBeInTheDocument();
   });
 
   test('no redirect button to new session page when missing exercises', () => {
-    renderWorkoutList(preloadedState);
+    renderWorkoutList(workouts);
 
     expect(screen.queryByText('Start workout')).not.toBeInTheDocument();
   });
