f26dee490eb6ec953d3a29f445c5ddf2e5d3ce9e
Przemyslaw Koziel <mos.koziel@gmail.com>
2024-09-12 08:34:52 +0200

    feature: starting new workout only if exercises present

--- src/app/features/workouts/WorkoutList.test.tsx
+++ src/app/features/workouts/WorkoutList.test.tsx
@@ -7,29 +7,7 @@ import { WorkoutList } from './WorkoutList';
 import { WorkoutValue } from '@/app/core/workouts/workouts-schema';
 import { generateWorkout } from '@/lib/test-utils';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
-
-const preloadedState = {
-  exercises: {
-    values: [],
-  },
-  workouts: {
-    values: [
-      generateWorkout({
-        id: '1',
-        position: 0,
-        name: 'Upper body',
-      }),
-    ],
-  },
-};
-
-const store = configureStore({
-  reducer: {
-    workouts: workoutsReducer,
-    exercises: exercisesReducer,
-  },
-  preloadedState,
-});
+import { RootState } from '@/store/store';
 
 const deleteWorkout = vi.fn();
 
@@ -40,43 +18,86 @@ vi.mock('@/app/features/workouts/DeleteWorkoutDialog', () => ({
   },
 }));
 
+const renderWorkoutList = (preloadedState: RootState) => {
+  const store = configureStore({
+    reducer: {
+      workouts: workoutsReducer,
+      exercises: exercisesReducer,
+    },
+    preloadedState,
+  });
+
+  render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={['/workouts']}>
+        <Routes>
+          <Route path="/workouts" element={<WorkoutList />} />
+          <Route path="/workouts/:id" element={<div>Mock Edit Workout</div>} />
+          <Route path="/sessions/new" element={<div>Mock New Session</div>} />
+        </Routes>
+      </MemoryRouter>
+    </Provider>,
+  );
+};
+
 describe('Workout list', () => {
+  let preloadedState: RootState;
+
   beforeEach(() => {
-    render(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={['/workouts']}>
-          <Routes>
-            <Route path="/workouts" element={<WorkoutList />} />
-            <Route
-              path="/workouts/:id"
-              element={<div>Mock Edit Workout</div>}
-            />
-            <Route path="/sessions/new" element={<div>Mock New Session</div>} />
-          </Routes>
-        </MemoryRouter>
-      </Provider>,
-    );
+    preloadedState = {
+      exercises: {
+        values: [],
+      },
+      workouts: {
+        values: [
+          generateWorkout({
+            id: '1',
+            position: 0,
+            name: 'Upper body',
+            exercises: [],
+          }),
+        ],
+      },
+    };
   });
 
   test('renders the list of workouts', () => {
+    renderWorkoutList(preloadedState);
+
     preloadedState.workouts.values.forEach((workouts) => {
       expect(screen.getByText(workouts.name)).toBeInTheDocument();
     });
   });
 
   test('pass through workouts to child components', async () => {
+    renderWorkoutList(preloadedState);
+
     expect(deleteWorkout).toHaveBeenCalledWith(
       preloadedState.workouts.values[0],
     );
   });
 
   test('redirect to edit page', () => {
+    renderWorkoutList(preloadedState);
+
     fireEvent.click(screen.getAllByLabelText('Edit workout')[0]);
     expect(screen.getByText('Mock Edit Workout')).toBeInTheDocument();
   });
 
   test('redirect to new session page for a given workout', () => {
+    preloadedState.workouts.values[0].exercises = [
+      { id: '1', exerciseId: '1', sets: 3 },
+    ];
+
+    renderWorkoutList(preloadedState);
+
     fireEvent.click(screen.getByLabelText('Start workout'));
     expect(screen.getByText('Mock New Session')).toBeInTheDocument();
+  });
+
+  test('no redirect button to new session page when missing exercises', () => {
+    renderWorkoutList(preloadedState);
+
+    expect(screen.queryByText('Start workout')).not.toBeInTheDocument();
   });
 });
--- src/app/features/workouts/WorkoutList.tsx
+++ src/app/features/workouts/WorkoutList.tsx
@@ -25,14 +25,16 @@ export const WorkoutList: React.FC = () => {
           >
             <h3 className="mr-auto truncate">{workout.name}</h3>
 
-            <Button asChild variant="icon" size="icon">
-              <Link
-                to={`/sessions/new?workoutId=${workout.id}`}
-                aria-label="Start workout"
-              >
-                <PlayIcon className="h-4 w-4" />
-              </Link>
-            </Button>
+            {workout.exercises.length > 0 && (
+              <Button asChild variant="icon" size="icon">
+                <Link
+                  to={`/sessions/new?workoutId=${workout.id}`}
+                  aria-label="Start workout"
+                >
+                  <PlayIcon className="h-4 w-4" />
+                </Link>
+              </Button>
+            )}
             <Button asChild variant="icon" size="icon">
               <Link to={`/workouts/${workout.id}`} aria-label="Edit workout">
                 <PencilIcon className="h-4 w-4" />
